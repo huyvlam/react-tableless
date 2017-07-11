d9b416caadd08fa1d358cfaa11f53c1524055861
Huy Lam <huyster@Huys-MacBook-Pro.local>
2017-07-10 20:43:05 -0700

    modify package.json, remove unneeded files

--- client/index.js
+++ client/index.js
@@ -1,9 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import App from './components/App.jsx'
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+export default from './components'
