7afc946cbdae508c4e008e3eebbdde2a3754956e
Huy Lam <huyster@Huys-MacBook-Pro.local>
2017-07-29 17:38:29 -0700

    v1.0.1 is a complete refactor of code, setup, config. fix output error. rewrite webpack.config, package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,59 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+const path = require('path')
+
+const APP_DIR = path.resolve(__dirname, 'src')
+const BUILD_DIR = path.resolve(__dirname, 'lib')
 
 module.exports = {
-  entry: './client/index.js',
+  entry: APP_DIR + '/index.js',
   output: {
-    path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    path: BUILD_DIR,
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'Tableless'
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+    'prop-types': 'prop-types'
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        include: APP_DIR,
+        options: {
+          presets: [
+            'es2015', 'react'
+          ]
+        }
       },
       {
         test: /\.jsx$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        include: APP_DIR,
+        options: {
+          presets: [
+            'es2015', 'react'
+          ]
+        }
       }
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './client/index.html',
-      filename: 'index.html',
-      inject: 'body'
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true
+      },
+      compress: {
+        warnings: false,
+        screw_ie8: true
+      }
     })
   ]
 }
